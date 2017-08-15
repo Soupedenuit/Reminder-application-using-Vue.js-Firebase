437b16de0bc8496a8ad2aeb5803b996cd13d6dcc
Tony Whomever <Soupedenuit@users.noreply.github.com>
2017-08-15 02:56:55 -0400

    delete buttons
    worked all day to add delete buttons.
    Making them work on proper list item was difficult.

--- vue_components.js
+++ vue_components.js
@@ -1,12 +1,12 @@
 /*******************************
  Composing with Vue Components:
-********************************
+*****************
  SESSION TIME:
- ******************************/
+*******************************/
 
 Vue.component('session-time', {
   //props: ['alpha'],
-  template: '<p>{{ alpha }}</p>',
+  template: '<p v-html="alpha"></p>',
   data: function() {
     return {alpha: 'session started!'};
   },
@@ -20,8 +20,8 @@ Vue.component('session-time', {
   }
 });
 
-var component1 = new Vue({
-  el: '#component1'
+var sessionTimeComponent = new Vue({
+  el: '#sessionTimeComponent'
 });
 
 
@@ -29,17 +29,23 @@ var component1 = new Vue({
  Vue Instances:
 *******************************/
 
-var instance1 = new Vue({
-  el: '#instance1',
-  data: {
-    welcomeMsg: 'Welcome to my Vue.js testing ground!'
-  },
+var todayDateInstance = new Vue({
+  el: '#todayDateInstance',
+  data: {},
   methods: {
     todayDate: function() {
       return "Today is " + todayDate();
     }
   }
 });
+
+
+var welcomeInstance = new Vue({
+  el: '#welcomeInstance',
+  data: {
+    welcomeMsg1: 'Welcome to my Vue.js testing ground!'
+  }
+})
 
 
 /*****************************************
@@ -51,23 +57,45 @@ Vue.component('todo-item', {
   template: '<li>{{ todo.text }}</li>'
 })
 
-var instance2 = new Vue({
-  el: '#instance2',
+var toDoInstance = new Vue({
+  el: '#toDoInstance',
   data: {
     toDoList: [
-      { id: 0, text: 'learn Vue.js' },
-      { id: 1, text: 'learn MongoDB' },
-      { id: 2, text: 'learn Kung Fu' }
-    ]
+      { id: 0, text: '<button type="button" class="del" onclick="deleteItem(0)" style="text-align: right;">x</button>learn Vue.js' },
+      { id: 1, text: '<button type="button" class="del" onclick="deleteItem(1)">x</button>learn MongoDB' },
+      { id: 2, text: '<button type="button" class="del" onclick="deleteItem(2)">x</button>learn Kung Fu' }
+    ],
+    project: 'a To-Do app project (work in progress). Try it out!'
   }
 });
 
 
+/*******************************
+ Functions to delete items:
+*******************************/
+
 document.getElementById("toDoItem").addEventListener("click", function() {
-  var newItem = addToList.value;
-  instance2.toDoList.push({ text: newItem });
+  var y = toDoInstance.toDoList.length;
+  /*
+  var newItem = addToList.value + '<input type="image" src="del_button.png" class="del" onclick="deleteItem(' + y + ')">del: ' + y + ' />';
+  */
+  var newItem = '<button type="button" class="del" onclick="deleteItem(' + y + ')">x</button>' + addToList.value;
+  toDoInstance.toDoList.push({ id: y, text: newItem });
   addToList.value = '';
 });
+
+function deleteItem(id) {
+  //var remove = this.text;
+  function findById(idNum) {
+    return idNum.id === id;
+  }
+
+  foundObj = toDoInstance.toDoList.find(findById);
+  indexOfFoundObj = toDoInstance.toDoList.indexOf(foundObj);
+  var remove = indexOfFoundObj;
+  //window.alert("removing item with id " + id);
+  toDoInstance.toDoList.splice(remove, 1);
+}
 
 /*
 document.getElementById("addToList").addEventListener("keydown", function(event) {
