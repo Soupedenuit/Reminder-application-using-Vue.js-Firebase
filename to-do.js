4840d8079240fe00fa619ef66e9fc4b2d9ae4fc2
Tony Whomever <Soupedenuit@users.noreply.github.com>
2017-09-09 15:17:14 -0400

    Add files via upload

--- to-do.js
+++ to-do.js
@@ -1,4 +1,15 @@
 /******************************************************
+*******************************************************
+**  Copyright (C) 2017
+**  AUTHOR:  Tony Whomever
+**   PLACE:  Ottawa, ON, Canada
+**    DATE:  September 2017
+**
+*******************************************************
+******************************************************/
+
+
+/******************************************************
  Composing with Vue Components - session-time custom:
 ******************************************************/
 
@@ -85,8 +96,9 @@ var toDoInstance = new Vue({
     //textitem1: 'learn MongoDB',
     //textitem2: 'learn Kung Fu',
     toDoList: [
-      { id: 0, text: '<span id="item' + 0 + '">you can add, move, delete and restore items</span><span><input type="checkbox" title="done" class="crossout" id="checkbox' + 0 + '" onclick="crossOutItem(' + 0 + ')"/><button type="button" title="move up" class="up" onclick="moveUp(' + 0 + ')">↑</button><button type="button" title="move down" class="down" onclick="moveDown(' + 0 + ')">↓</button><button type="button" title="delete" class="del" onclick="deleteItem(' + 0 + ')">x</button></span>' },
-      { id: 1, text: '<span id="item' + 1 + '" contenteditable="true" spellcheck="false">you can also edit an item (try it)</span><span><input type="checkbox" title="done" class="crossout" id="checkbox' + 1 + '" onclick="crossOutItem(' + 1 + ')"/><button type="button" title="move up" class="up" onclick="moveUp(' + 1 + ')">↑</button><button type="button" title="move down" class="down" onclick="moveDown(' + 1 + ')">↓</button><button type="button" title="delete" class="del" onclick="deleteItem(' + 1 + ')">x</button></span>' }
+      { id: 0, text: '<span id="item' + 0 + '" contenteditable="true" spellcheck="false">add, edit, move, delete and restore items</span><span><input type="checkbox" title="done" class="crossout" id="checkbox' + 0 + '" onclick="crossOutItem(' + 0 + ')"/><button type="button" title="move up" class="up" onclick="moveUp(' + 0 + ')">↑</button><button type="button" title="move to top" class="up top" onclick="moveToTop(' + 0 + ')">↑↑</button><button type="button" title="move down" class="down" onclick="moveDown(' + 0 + ')">↓</button><button type="button" title="move to bottom" class="down bottom" onclick="moveToBottom(' + 0 + ')">↓↓</button><button type="button" title="delete" class="del" onclick="deleteItem(' + 0 + ')">x</button></span>' },
+      { id: 1, text: '<span id="item' + 1 + '" contenteditable="true" spellcheck="false">added items will be saved in local storage (excluding restored)</span><span><input type="checkbox" title="done" class="crossout" id="checkbox' + 1 + '" onclick="crossOutItem(' + 1 + ')"/><button type="button" title="move up" class="up" onclick="moveUp(' + 1 + ')">↑</button><button type="button" title="move to top" class="up top" onclick="moveToTop(' + 1 + ')">↑↑</button><button type="button" title="move down" class="down" onclick="moveDown(' + 1 + ')">↓</button><button type="button" title="move to bottom" class="down bottom" onclick="moveToBottom(' + 1 + ')">↓↓</button><button type="button" title="delete" class="del" onclick="deleteItem(' + 1 + ')">x</button></span>' },
+      { id: 2, text: '<span id="item' + 2 + '" contenteditable="true" spellcheck="false">clicking the logo on the left will clear local storage</span><span><input type="checkbox" title="done" class="crossout" id="checkbox' + 2 + '" onclick="crossOutItem(' + 2 + ')"/><button type="button" title="move up" class="up" onclick="moveUp(' + 2 + ')">↑</button><button type="button" title="move to top" class="up top" onclick="moveToTop(' + 2 + ')">↑↑</button><button type="button" title="move down" class="down" onclick="moveDown(' + 2 + ')">↓</button><button type="button" title="move to bottom" class="down bottom" onclick="moveToBottom(' + 2 + ')">↓↓</button><button type="button" title="delete" class="del" onclick="deleteItem(' + 2 + ')">x</button></span>' }
     ],
     project: '<p>To-Do app project.</p>',
     anotherList: [
@@ -94,7 +106,14 @@ var toDoInstance = new Vue({
       { name: "karen", age: "also 44"},
       { name: "mia", age: 3}
     ],
-    deletedList: []
+    deletedList: [],
+    dynamicCSS: false
+  },
+  computed: {
+    listLength: function() {
+      var itemsCount = this.toDoList.length;
+      return itemsCount;
+    }
   }
 });
 
@@ -113,16 +132,43 @@ document.getElementById("toDoItem").addEventListener("click", function() {
 */
 
 // Version 1.1
-var itemCounter = 2;
+
+// Function to get item id by number only:
+function idNumberIsolator(idWithLetters) {
+  var y = new RegExp(/\d/g);
+  var numbersOnly = idWithLetters.match(y).join('');
+  var idDigitsToNumber = Number(numbersOnly)
+  return idDigitsToNumber;
+}
+
+// Check if there is a stored item in localStorage
+//  & set the itemCounter accordingly:
+var lastStoredId = localStorage.getItem("lastStoredKey");
+if ( lastStoredId ) {
+  var lastStoredIdNumber = idNumberIsolator(lastStoredId);
+  var itemCounter = lastStoredIdNumber + 1;
+}
+else var itemCounter = 3;
+
+// Action when item is added:
 document.getElementById("toDoItem").addEventListener("click", function() {
   var y = itemCounter;
-  var newItem = '<span id="item' + y + '" contenteditable="true" spellcheck="false">' + addToList.value + '</span><span><input type="checkbox" title="done" class="crossout" id="checkbox' + y + '" onclick="crossOutItem(' + y + ')"/><button type="button" title="move up" class="up" onclick="moveUp(' + y + ')">↑</button><button type="button" title="move down" class="down" onclick="moveDown(' + y + ')">↓</button><button type="button" title="delete" class="del" onclick="deleteItem(' + y + ')">x</button></span>';
+  var newItem = '<span id="item' + y + '" contenteditable="true" spellcheck="false">' + addToList.value + '</span><span><input type="checkbox" title="done" class="crossout" id="checkbox' + y + '" onclick="crossOutItem(' + y + ')"/><button type="button" title="move up" class="up" onclick="moveUp(' + y + ')">↑</button><button type="button" title="move to top" class="up top" onclick="moveToTop(' + y + ')">↑↑</button><button type="button" title="move down" class="down" onclick="moveDown(' + y + ')">↓</button><button type="button" title="move to bottom" class="down bottom" onclick="moveToBottom(' + y + ')">↓↓</button><button type="button" title="delete" class="del" onclick="deleteItem(' + y + ')">x</button></span>';
   /* Doesn't work:
   document.getElementById("item" + y).setAttribute('title', 'just do it!');
   */
-  toDoInstance.toDoList.push({ id: y, text: newItem });
+  var textValue = { id: y, text: newItem };
+  toDoInstance.toDoList.push(textValue);
+  //console.log("newItem:  " + newItem);
   addToList.value = '';
   itemCounter++;
+
+  // Store items in localStorage:
+  var x = "savedListItem" + y;
+  localStorage.setItem(x, newItem);
+
+  // Keep track of last saved item:
+  localStorage.setItem("lastStoredKey", x);
 });
 
 
@@ -136,6 +182,7 @@ document.getElementById("toDoItem").addEventListener("click", function() {
 /******************************************************
  Delete items from the list:
 ******************************************************/
+
 var deletedItemsCount = 0;
 function deleteItem(id) {
   //var remove = this.text;
@@ -145,13 +192,15 @@ function deleteItem(id) {
   itemObj = toDoInstance.toDoList.find(itemObjectById);
   indexOfItemObj = toDoInstance.toDoList.indexOf(itemObj);
   toDoInstance.toDoList.splice(indexOfItemObj, 1);
-  console.log("itemObj:  " + toDoInstance.toDoList.itemObj);
+  //console.log("itemObj:  " + itemObj.text);
   toDoInstance.deletedList.push(itemObj);
   deletedItemsCount++;
   if ( deletedItemsCount == 1 ) {
     document.getElementById("restore").innerHTML = deletedItemsCount + " deleted item to restore";
   }
   else document.getElementById("restore").innerHTML = deletedItemsCount + " deleted items to restore";
+  var x = "savedListItem" + id;
+  localStorage.removeItem(x);
   /* try using this?  toDoInstance.toDoList.$remove(itemObj);
   */
 }
@@ -193,6 +242,47 @@ function moveDown(id) {
   else {
   toDoInstance.toDoList.splice(indexOfItemObj,1);
   var newIndexOfObject = indexOfItemObj + 1;
+  toDoInstance.toDoList.splice(newIndexOfObject,0,itemObj);
+  }
+}
+
+
+/******************************************************
+ Move items to top & bottom of the list:
+******************************************************/
+
+function moveToTop(id) {
+  //var remove = this.text;
+  function itemObjectById(item) {
+    return item.id === id;
+  }
+  itemObj = toDoInstance.toDoList.find(itemObjectById);
+  indexOfItemObj = toDoInstance.toDoList.indexOf(itemObj);
+  if ( indexOfItemObj == 0 ) {
+    window.alert("already at the top");
+  }
+  else {
+  toDoInstance.toDoList.splice(indexOfItemObj,1);
+  var newIndexOfObject = 0;
+  toDoInstance.toDoList.splice(newIndexOfObject,0,itemObj);
+  }
+}
+
+
+function moveToBottom(id) {
+  //var remove = this.text;
+  function itemObjectById(item) {
+    return item.id === id;
+  }
+  itemObj = toDoInstance.toDoList.find(itemObjectById);
+  indexOfItemObj = toDoInstance.toDoList.indexOf(itemObj);
+  var lastIndex = toDoInstance.toDoList.length - 1;
+  if ( indexOfItemObj == lastIndex ) {
+    window.alert("already at the bottom");
+  }
+  else {
+  toDoInstance.toDoList.splice(indexOfItemObj,1);
+  var newIndexOfObject = lastIndex;
   toDoInstance.toDoList.splice(newIndexOfObject,0,itemObj);
   }
 }
@@ -305,21 +395,12 @@ $( document ).ready(function() { $("#addToList").on('keydown', function(event){
  Misc functions:
 ******************************************************/
 
-// Regex Object to get checkbox by number only:
-// (currently unused)
-function checkboxNumberFinder(id) {
-  var y = new RegExp(/\d/g);
-  var word = id;
-  var checkboxNumber = word.match(y).join('');
-}
-
-
 // Get "enter" key to click checkbox:
-// XX-UNRESOLVED!
-$( document ).ready(function() {  $(".crossout").on('keydown', function(event){
+// XX-NOT WORKING => UNRESOLVED!
+$( document ).ready(function() {  $("input[type=checkbox]").on('keypress', function(event){
     if ( event.keyCode === 13 ) {
-        event.preventDefault();
-        alert('yo');
+      event.preventDefault();
+      $(this).click();
     }
   });
 });
@@ -333,10 +414,31 @@ $( document ).ready(function() {
 });
 
 
+// Change theme (color, text of h1 and p):
+document.getElementById("switch-theme").addEventListener("click", function() {
+  document.querySelector('nav').style.cssText = "color: #707ac0;";
+  for ( i = 0; i < document.getElementsByTagName('button').length; i++ ) {
+    document.getElementsByTagName('button')[i].style.cssText = "background-color: #707ac0;";
+  }
+  welcomeInstance.welcomeMsg1 = "Today will be awesome!";
+  document.querySelector('h1').innerHTML = "NAC Parking Services";
+  document.getElementById("logo1").src = "assets/checkmark_NAC_colour.svg";
+
+  // // XX-NOT WORKING => UNRESOLVED!:
+  // identify my stylesheet (to-do.css):
+  //var myStylesheet = document.styleSheets[0];
+  // find length of my stylesheet (# of rules):
+  //var numberOfStylesheetRules = myStylesheet.cssRules.length;
+  // Inserts a rule at the end of my Stylesheet:
+  //stylesheet.insertRule("button { background-color: #707ac0;}", numberOfStylesheetRules);
+});
+
+
 /******************************************************
  CUSTOM ELEMENT method 1 (named created-ago):
 ******************************************************/
 
+/*
 var Custom1 = Object.create(HTMLElement.prototype);
 
 Custom1.createdCallback = function() {
@@ -346,3 +448,35 @@ Custom1.createdCallback = function() {
 document.registerElement("created-ago", {
   prototype: Custom1
 });
+*/
+
+
+/******************************************************
+ Retrieve localStorage items:
+******************************************************/
+
+if ( localStorage.length > 0 ) {
+  document.addEventListener('DOMContentLoaded',function() {
+    for ( i = 3; i <= lastStoredIdNumber; i++ ) {
+      var beta = localStorage.getItem('savedListItem' + i);
+      if ( beta ) {
+        toDoInstance.toDoList.push({ id: i, text: beta });
+      }
+    }
+  });
+}
+
+  document.getElementById("logo1").addEventListener("click", function() {
+    localStorage.clear();
+  });
+
+
+  /******************************************************
+  *******************************************************
+  **  Copyright (C) 2017
+  **  AUTHOR:  Tony Whomever
+  **   PLACE:  Ottawa, ON, Canada
+  **    DATE:  September 2017
+  **
+  *******************************************************
+  ******************************************************/
